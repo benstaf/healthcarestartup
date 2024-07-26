74bd69bbe8a21adc783e7dd98b9e21fed0902f15
benstaf <mostafacebook@gmail.com>
2024-07-26 19:31:22 +0200

    Update index.js yandex metrica

--- components/hero/index.js
+++ components/hero/index.js
@@ -23,5 +23,7 @@ export default () => (
         alt="illustration"
       />
     </div>
+   <div><img src="https://mc.yandex.ru/watch/97927750" style="position:absolute; left:-9999px;" alt="" /></div>
+
   </section>
 );
